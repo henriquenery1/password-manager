766188064d4c1af704d4c35cadd11d2045eedc79
Henrique Nery <henriquenery07@gmail.com>
2022-07-30 19:24:58 -0300

    feature: create crud for passwords

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PasswordsModule } from './passwords/passwords.module';
 
 @Module({
-  imports: [],
+  imports: [PasswordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
